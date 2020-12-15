c10b67a72f6e5ac2309a46c5278b5c7c5290c316
David Yeung <quxbaz@gmail.com>
2020-12-15 13:19:43 -0700

    Initial commit.

---
+++ src/Modal/Modal.js
@@ -0,0 +1,27 @@
+import css from './style.css'
+import React from 'react'
+import {createPortal} from 'react-dom'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { useEscapeKey } from 'hooks'
+
+const Modal = ({children, className, style, onClose}) => {
+  useEscapeKey(onClose)
+  return createPortal(
+    <>
+      <div className={css.Backdrop} onClick={onClose} />
+      <div className={classNames(css.Modal, className)} style={style}>
+        {children}
+      </div>
+    </>,
+    document.getElementById('Modals')
+  )
+}
+
+Modal.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  onClose: PropTypes.func,
+}
+
+export default Modal
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const abs = (...args) => path.resolve(__dirname, ...args)
+
+module.exports = (env, argv) => ({
+
+  devtool: 'source-map',
+
+  output: argv.mode === 'production' ? {
+    filename: 'fs-modal.js',
+    path: abs('public/'),
+    library: 'fs-modal',
+    libraryTarget: 'umd',
+  } : {
+    filename: 'fs-modal.js',
+  },
+
+  devServer: {
+    contentBase: abs('public/'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          abs('./src'),
+        ],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?{"modules": {"localIdentName": "[path][name]__[local]"}}',
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    modules: [
+      abs('node_modules'),
+    ],
+  },
+
+})
